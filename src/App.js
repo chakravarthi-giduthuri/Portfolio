8154b6abcf45562f38c4d66829d12469eb640464
chakravarthi <68289097+chakravarthi-giduthuri@users.noreply.github.com>
2021-02-06 18:58:13 +0530

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import "./App.css";
 import Home from "./components/Home";
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/About";
 import Projects from "./components/Projects";
 // import Nav from "./components/Nav";
 function App() {
-  
   return (
     <Router>
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects}  />
+          <Route path="/projects" component={Projects} />
         </Switch>
       </div>
     </Router>
