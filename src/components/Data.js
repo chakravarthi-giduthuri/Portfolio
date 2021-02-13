435f4f889c5b7e5c1680ee78622de15fd9a8848e
chakravarthi <68289097+chakravarthi-giduthuri@users.noreply.github.com>
2021-02-13 11:34:54 +0530

    ADD python projects

--- src/components/Data.js
+++ src/components/Data.js
@@ -11,9 +11,29 @@ const projects = [
     id: 2,
     title: "djangologin",
     category: "react",
-    img: "./images/js.png",
+    img: "./images/reactjs.png",
     github: "https://github.com/chakravarthi-giduthuri/react-task-tracker",
     link: "https://todo-task-tracker.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "banking system",
+    category: "python",
+    img: "./images/Python.jpg",
+    github:
+      "https://github.com/chakravarthi-giduthuri/Python-Projects/blob/main/banking.py",
+    link:
+      "https://github.com/chakravarthi-giduthuri/Python-Projects/blob/main/banking.py",
+  },
+  {
+    id: 4,
+    title: "phonebook",
+    category: "python",
+    img: "./images/Python.jpg",
+    github:
+      "https://github.com/chakravarthi-giduthuri/Python-Projects/blob/main/phonebook.py",
+    link:
+      "https://github.com/chakravarthi-giduthuri/Python-Projects/blob/main/phonebook.py",
   },
 ];
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -6,6 +6,37 @@ function Project({ Projects }) {
     <div>
       <div>
         <div className="head">
+          <h2 className="title">Python</h2>
+          <div className="under">
+            <div className="underline"></div>
+            <div className="dot"></div>
+            <div className="underline"></div>
+          </div>
+        </div>
+        <div className="project-card">
+          {Projects.map((project) => {
+            if (project.category === "python") {
+              return (
+                <article key={project.id} className="art">
+                  <img className="photo" src={project.img} alt="python" />
+                  <div className="icons">
+                    <a href={project.github}>
+                      <CodeOutlined style={{ fontSize: 30, marginRight: 20 }} />
+                    </a>
+                    <a href={project.link}>
+                      <LinkOutlined style={{ fontSize: 30 }} />
+                    </a>
+                  </div>
+                </article>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+      <div>
+        <div className="head">
           <h2 className="title">django</h2>
           <div className="under">
             <div className="underline"></div>
